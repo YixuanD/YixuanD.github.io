212f5f235373d61795da124087c349a84032d9a2
YixuanD <Ding.yixu@northeastern.edu>
2021-12-14 22:38:47 -0500

    final7

--- homework/final project/chart1.js
+++ homework/final project/chart1.js
@@ -1,13 +1,13 @@
 d3.select("#chart1")
-  .on("mousemove", function(d) {
+  .on("mousemove", function() {
 
     var tooltip = d3.select("#tooltip")
       .style("display", "block")
       .style("top", d3.event.pageY + 20 + "px")
       .style("left", d3.event.pageX + 20 + "px");
 
-    tooltip.select("#title").html("Artists:" + d.data.name);
-    tooltip.select("#paintings").html("Paintings:" + d.data.paintings);
+    tooltip.select("#title").html("Artists and Paintings");
+    tooltip.select("#paintings").html("Artists: 1-50");
 
   })
 
--- homework/final project/chart2.js
+++ homework/final project/chart2.js
@@ -1,13 +1,13 @@
 d3.select("#chart2")
-  .on("mousemove", function(d) {
+  .on("mousemove", function() {
 
     var tooltip = d3.select("#tooltip")
       .style("display", "block")
       .style("top", d3.event.pageY + 20 + "px")
       .style("left", d3.event.pageX + 20 + "px");
 
-    tooltip.select("#title").html("Genre:" + d.data.genre);
-    tooltip.select("#paintings").html("Paintings:" + d.data.paintings);
+    tooltip.select("#title").html("Genre and Paintings:");
+    tooltip.select("#paintings").html("Paintings: 1-877");
 
   })
 
