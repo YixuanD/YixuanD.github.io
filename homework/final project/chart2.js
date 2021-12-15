24398fa242d1149b545a3e3346f686a86c0abd5b
YixuanD <Ding.yixu@northeastern.edu>
2021-12-14 23:08:17 -0500

    final7

--- homework/final project/chart2.js
+++ homework/final project/chart2.js
@@ -6,7 +6,7 @@ d3.select("#chart2")
       .style("top", d3.event.pageY + 20 + "px")
       .style("left", d3.event.pageX + 20 + "px");
 
-    tooltip.select("#title").html("Genre and Paintings:");
+    tooltip.select("#title").html("Genre and Paintings");
     tooltip.select("#paintings").html("Paintings: 1-877");
 
   })
